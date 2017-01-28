99732d95322f04500f0a615aa7aaf010dcf54e97
Bruce <bruce.galpin@gmail.com>
2017-01-28 15:27:12 +0200

    Completed the Header

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
 	browserSync.init({
 		notify: false,
@@ -11,17 +11,25 @@ gulp.task('watch', function(){
 		}
 	});
 
-	watch('./app/index.html', function(){
+	watch('./app/index.html', function() {
 		browserSync.reload();
 	});
 
-	watch('./app/assets/styles/**/*.css', function(){
+	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
+	});
+
+	watch('./app/assets/scripts/**/*.js', function() {
+		gulp.start('scriptsRefresh');
 	});
 
 });
 
-gulp.task('cssInject', ['styles'], function(){
+gulp.task('cssInject', ['styles'], function() {
 	return gulp.src('./app/temp/styles/style.css')
 		.pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+	browserSync.reload();
+})
